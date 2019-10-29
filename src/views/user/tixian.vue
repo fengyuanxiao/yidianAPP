14c95561e662c859404d7527e2524a6b091f4ece
罗兰芬 <radish122@163.com>
2019-10-29 13:57:49 +0800

    审核弹窗

--- src/views/user/tixian.vue
+++ src/views/user/tixian.vue
@@ -110,6 +110,18 @@
           <x-button type="primary" style="border-radius:5px;background:#1890ff;width:35%;" min>去绑定</x-button>
         </div> -->
       </x-dialog>
+    <!-- 身份证弹窗 -->
+     <x-dialog v-model.trim="showPop" class="dialog_demo">
+        <group title>
+          <p class="showAttention">提示</p>
+        </group>
+        <div class="img-box showBg">
+          <p style="font-size: 18px;color: black;padding: 25px 0px 15px;">您的银行卡或身份证正在审核，请耐心等待！</p>
+        </div>
+        <div @click="showPop=false" style="margin: 30px 0 10px 0;">
+          <x-button type="primary" style="border-radius:5px;background:#1890ff;width:35%;" min>确定</x-button>
+        </div>
+      </x-dialog>
   </div>
 </template>
 <script>
@@ -124,6 +136,7 @@ export default {
   data() {
     return {
       showID:false,
+      showPop:false,
       showBank:false,
       realnameStatus:null,
       count: 60,
@@ -202,6 +215,9 @@ export default {
           this.showID=true
         }else{
           this.showID=false
+      }
+      if((this.userCenterInfo.realname_status==2 && this.userCenterInfo.bank_status==1) || (this.userCenterInfo.bank_status==2 && this.userCenterInfo.realname_status==1)|| (this.userCenterInfo.bank_status==2 && this.userCenterInfo.realname_status==2)){
+          this.showPop=true 
       }
     },
     async tixian() {
